45b916019fe9d9d70995ddfea89af302d267d64c
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-10-21 22:12:55 -0300

    wip: mask event listener

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,20 @@
+import { Masks } from "./modules/Masks.js";
+
+const masks = {
+  name: Masks.nameMask,
+  cpf: Masks.cpfMask,
+  birthDate: Masks.birthDateMask,
+  phone: Masks.phoneMask,
+  cep: Masks.cepMask,
+};
+
 document.querySelectorAll("input").forEach(($input) => {
-  const field = $input.dataset.js;
+  const fieldToMask = masks[$input.id];
 
   $input.addEventListener(
     "input",
     (e) => {
-      e.target.value = masks[field](e.target.value);
+      e.target.value = fieldToMask(e.target.value);
     },
     false
   );
